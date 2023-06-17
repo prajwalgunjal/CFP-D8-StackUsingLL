3a1fd7b9df369af88c11638ba35f63bb8c4e9086
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-06-17 11:12:40 +0530

    Add TO stack

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace StackUsingLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int> ();
+            stack.add (1);
+            stack.add (2);
+            stack.add (3);
+            stack.add (4);
+            stack.add (5);
+            stack.add (6);
+            stack.add (7);
+            stack.Display ();
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingLL
+{
+    internal class Stack<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        public void add(T Data) {
+            Node<T> newNode = new Node<T>(Data);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        public void Display()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("Stack is empty !!!!");
+            }
+            else
+            {
+                Node < T > temp = head;
+                while(temp != null)
+                {
+                    Console.Write(temp.Data+" -> ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
